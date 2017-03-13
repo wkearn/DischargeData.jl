1a69b37a0ea3242b755915cabae0b5a96cafe035
Will Kearney <wkearn@gmail.com>
2017-03-13 15:50:36 -0400

    Move the DischargeData type from TDM
    And rename

--- src/DischargeData.jl
+++ src/DischargeData.jl
@@ -1,5 +1,15 @@
 module DischargeData
 
-# package code goes here
+export Discharge
+
+type Discharge
+    cp::Vector{Float64}
+    ts::Vector{DateTime}
+    vs::Vector{Float64}
+    A::Vector{Float64}
+    Q::Vector{Float64}
+end
+
+Discharge(cp::Vector{Float64},Q::Vector{Float64}) = Discharge(cp,DateTime[],Float64[],Float64[],Q)
 
 end # module
