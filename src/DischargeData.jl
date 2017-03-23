6672b5890040e1034adf3337ed79f6c024052978
Will Kearney <wkearn@gmail.com>
2017-03-23 18:40:00 -0400

    Add a Calibration type
    This seems like the most appropriate place for it

--- src/DischargeData.jl
+++ src/DischargeData.jl
@@ -1,6 +1,6 @@
 module DischargeData
 
-export Discharge
+export Discharge, Calibration
 
 type Discharge
     cp::Vector{Float64}
@@ -11,5 +11,14 @@ type Discharge
 end
 
 Discharge(cp::Vector{Float64},Q::Vector{Float64}) = Discharge(cp,DateTime[],Float64[],Float64[],Q)
+
+##################################################
+# A light type for holding Calibration data
+
+type Calibration
+    t::Vector{DateTime}
+    Q::Vector{Float64}
+    dd::Discharge
+end
 
 end # module
