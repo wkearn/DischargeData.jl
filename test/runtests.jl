145b5aae58759333f32721121f4ac36a44c8943d
William Kearney <wkearn@gmail.com>
2017-03-28 15:35:32 -0400

    Add more tests for the other Quantitys

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,16 @@ hh = Stage(collect(now():now()+Day(1)),randn(2))
 
 cstage = Calibration(ht,hh)
 
+At = CrossSectionalArea(collect(now():now()+Day(1)),randn(2))
+AA = CrossSectionalArea(collect(now():now()+Day(1)),randn(2))
+
+carea = Calibration(At,AA)
+
+vt = Velocity(collect(now():now()+Day(1)),[(randn(3)...);(randn(3)...)])
+vv = Velocity(collect(now():now()+Day(1)),[(randn(3)...);(randn(3)...)])
+
+cvelocity = Calibration(vt,vv)
+
 dd = Discharge(collect(now():now()+Day(1)),randn(2))
 dt = Discharge(collect(now():now()+Day(1)),randn(2))
 
