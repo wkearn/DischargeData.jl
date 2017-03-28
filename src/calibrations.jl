066c85e7a92711f92e0ada4c80c050cdb7af4153
Will Kearney <wkearn@gmail.com>
2017-03-28 16:50:09 -0400

    Change some names

--- src/calibrations.jl
+++ src/calibrations.jl
@@ -1,15 +1,15 @@
 """
 A Calibration holds two TidalFluxQuantities
 
-- qtrue is the 'true' quantity that we want to calibrate to
-- qmeas is the measured quantity that we want to calibrate from
+- qto is the 'true' quantity that we want to calibrate to
+- qfrom is the measured quantity that we want to calibrate from
 """
 type Calibration
-    qtrue::Quantity
-    qmeas::Quantity
+    qto::Quantity
+    qfrom::Quantity
 end
 
-true_quantities(c::Calibration) = c.qtrue
-meas_quantities(c::Calibration) = c.qmeas
+to_quantity(c::Calibration) = c.qtrue
+from_quantity(c::Calibration) = c.qmeas
 
-times(c::Calibration) = (times(c.qtrue),times(c.qmeas))
+times(c::Calibration) = (times(to_quantity(c)),times(from_quantity(c)))
