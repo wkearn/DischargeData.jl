806eb699d793d5e39931679c4561bab89da40a7c
Will Kearney <wkearn@gmail.com>
2017-03-28 11:14:18 -0400

    Create TFQ abstract vector and subtype Discharge
    This also creates a new Stage type so that each
    TFQ only represents one quantity

--- src/DischargeData.jl
+++ src/DischargeData.jl
@@ -2,21 +2,9 @@ module DischargeData
 
 export Discharge, Calibration
 
-"""
-    Discharge(cp,ts,vs,A,Q)
+using Base.Dates
 
-A type to hold all the stuff we calculate when we 
-calculate discharge time series
-"""
-type Discharge
-    cp::Vector{Float64}
-    ts::Vector{DateTime}
-    vs::Vector{Float64}
-    A::Vector{Float64}
-    Q::Vector{Float64}
-end
-
-Discharge(cp::Vector{Float64},Q::Vector{Float64}) = Discharge(cp,DateTime[],Float64[],Float64[],Q)
+include("quantities.jl")
 
 ##################################################
 # A light type for holding Calibration data
