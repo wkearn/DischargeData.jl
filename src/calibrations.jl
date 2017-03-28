c95ac931d8a364f3068c6eb78c137f64b5ac2bf0
Will Kearney <wkearn@gmail.com>
2017-03-28 11:18:07 -0400

    Create a new, generic calibration type

---
+++ src/calibrations.jl
@@ -0,0 +1,15 @@
+"""
+A Calibration holds two TidalFluxQuantities
+
+- qtrue is the 'true' quantity that we want to calibrate to
+- qmeas is the measured quantity that we want to calibrate from
+"""
+type Calibration
+    qtrue::TidalFluxQuantity
+    qmeas::TidalFluxQuantity
+end
+
+true_quantities(c::Calibration) = c.qtrue
+meas_quantities(c::Calibration) = c.qmeas
+
+times(c::Calibration) = (times(c.qtrue),times(c.qmeas))
