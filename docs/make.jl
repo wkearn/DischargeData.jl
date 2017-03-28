96b15efce8325f2cb85869c07cc7c6b01ffaf553
William Kearney <wkearn@gmail.com>
2017-03-28 15:07:28 -0400

    Add math dependency

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter, DischargeData
 
 makedocs()
 
-deploydocs(deps=Deps.pip("mkdocs"),
+deploydocs(deps=Deps.pip("mkdocs","python-markdown-math"),
            repo="github.com/wkearn/DischargeData.jl.git",
            julia = "release"
 )
