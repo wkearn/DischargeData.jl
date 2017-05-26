b0bfa08920233243198e5166f4c5d8d6ee2a8a7a
Will Kearney <wkearn@gmail.com>
2017-05-26 10:02:10 -0400

    Make Fluxes Real not AbstractFloat

--- src/fluxes.jl
+++ src/fluxes.jl
@@ -1,17 +1,17 @@
 """
 # A flux of a scalar quantity is itself a quantity
 # """
-type Flux{S<:AbstractFloat,T<:Quantity{S}} <: Quantity{S}
+type Flux{S<:Real,T<:Quantity{S}} <: Quantity{S}
     ts::Vector{DateTime}
     q::Vector{S}
 end
 
-function Base.:*{S<:AbstractFloat,T<:Quantity{S}}(a::T,b::Discharge)
+function Base.:*{S<:Real,T<:Quantity{S}}(a::T,b::Discharge)
     times(a) == times(b) || error("Time series are not sampled at the same times. Consider interpolating")
     Flux{S,T}(times(a),quantity(a).*quantity(b))
 end
 
-function Base.:*{S<:AbstractFloat,T<:Quantity{S}}(a::Discharge,b::T)
+function Base.:*{S<:Real,T<:Quantity{S}}(a::Discharge,b::T)
     times(a) == times(b) || error("Time series are not sampled at the same times. Consider interpolating")
     Flux{S,T}(times(a),quantity(a).*quantity(b))
 end
