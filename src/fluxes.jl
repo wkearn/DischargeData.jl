989b9f698e55d8ef667d5d5fba872888cdfcbbf0
Will Kearney <wkearn@gmail.com>
2017-05-26 09:27:30 -0400

    Update Fluxes so they work with the measurements
    This will only work on v0.6 because it depends on
    the fancier parametric types

--- src/fluxes.jl
+++ src/fluxes.jl
@@ -1,14 +1,17 @@
 """
-A flux of a scalar quantity is itself a quantity
-"""
-@quantity Flux{T<:Quantity{Float64}} Float64
-
-function Base.:*{T<:Quantity{Float64}}(a::T,b::Discharge)
-    times(a) == times(b) || error("Time series are not sampled at the same times. Consider interpolating")
-    Flux{T}(times(a),quantity(a).*quantity(b))
+# A flux of a scalar quantity is itself a quantity
+# """
+type Flux{S<:AbstractFloat,T<:Quantity{S}} <: Quantity{S}
+    ts::Vector{DateTime}
+    q::Vector{S}
 end
 
-function Base.:*{T<:Quantity{Float64}}(a::Discharge,b::T)
+function Base.:*{S<:AbstractFloat,T<:Quantity{S}}(a::T,b::Discharge)
     times(a) == times(b) || error("Time series are not sampled at the same times. Consider interpolating")
-    Flux{T}(times(a),quantity(a).*quantity(b))
+    Flux{S,T}(times(a),quantity(a).*quantity(b))
+end
+
+function Base.:*{S<:AbstractFloat,T<:Quantity{S}}(a::Discharge,b::T)
+    times(a) == times(b) || error("Time series are not sampled at the same times. Consider interpolating")
+    Flux{S,T}(times(a),quantity(a).*quantity(b))
 end
