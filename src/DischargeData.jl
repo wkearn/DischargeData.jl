47f228ab59320c90c0e2450c5acffe610f6151ab
Will Kearney <wkearn@gmail.com>
2017-06-28 16:12:45 -0400

    Export TSS

--- src/DischargeData.jl
+++ src/DischargeData.jl
@@ -1,21 +1,22 @@
 module DischargeData
 
 export Quantity,
-Discharge,
-Stage,
-CrossSectionalArea,
-Velocity,
-AlongChannelVelocity,
-Temperature,
-Calibration,
-times,
-quantity,
-unzip,
-@quantity,
-@quantity_op,
-to_quantity,
-from_quantity,
-Flux
+    Discharge,
+    Stage,
+    CrossSectionalArea,
+    Velocity,
+    AlongChannelVelocity,
+    Temperature,
+    TSS,
+    Calibration,
+    times,
+    quantity,
+    unzip,
+    @quantity,
+    @quantity_op,
+    to_quantity,
+    from_quantity,
+    Flux
 
 using Base.Dates
 
